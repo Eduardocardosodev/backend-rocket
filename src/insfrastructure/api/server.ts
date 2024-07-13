401d636aedb4a3bf1e41f7fbb2bbeb5040a29af8
Eduardocardosodev <eduardocardoso.dev@gmail.com>
2024-07-13 12:58:42 -0300

    build: :green_heart: changing port

--- src/insfrastructure/api/server.ts
+++ src/insfrastructure/api/server.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import { app } from './app';
 
 dotenv.config();
-const port: number = Number(process.env.PORT) || 3000;
+const port: number = Number(process.env.PORT) || 3001;
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port} `);
